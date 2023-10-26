7671bb0e378d35a2e5dc7f7c7816cd1abc4c7f25
Bao Bui <baokhanhtk129@gmail.com>
2023-10-26 10:15:57 -0400

    Add Data Feature

---
+++ MyHealth/Models/HealthCategory.swift
@@ -0,0 +1,8 @@
+//
+//  HealthCategory.swift
+//  MyHealth
+//
+//  Created by Bao Bui on 10/18/23.
+//
+
+import Foundation
---
+++ MyHealth/ViewModels/ViewModels.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModels.swift
+//  MyHealth
+//
+//  Created by Bao Bui on 10/19/23.
+//
+
+import Foundation
